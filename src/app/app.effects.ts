2127717b365dceb60f23d5b3ab8f8b910cadbd5a
Stanislav Pavenko <pavenko_sv@mail.ru>
2020-08-16 14:25:36 +0300

    Implement ngRx effect

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { map } from 'rxjs/operators';
+import {
+  countActionsType,
+  CountUpdatedAtAction,
+} from './reducers/count/count.actions';
 
 @Injectable()
 export class AppEffects {
   constructor(private actions$: Actions) {}
+
+  @Effect()
+  updatedAt$() {
+    return this.actions$.pipe(
+      ofType(
+        countActionsType.increase,
+        countActionsType.decrease,
+        countActionsType.clear
+      ),
+      map(() => {
+        return new CountUpdatedAtAction({ updatedAt: Date.now() });
+      })
+    );
+  }
 }
